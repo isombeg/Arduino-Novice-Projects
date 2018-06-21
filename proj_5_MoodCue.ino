86a453f1eb12294c011d828ceb72a28fcd431c21
Guy-Jacques Isombe <isombeg@mcmaster.ca>
2018-06-20 23:10:41 -0400

    Add files via upload

---
+++ proj_5_MoodCue.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+Servo myServo;
+
+const int POTENT_PIN = A0;
+float potentRead = 0;
+int servoAngle = 0;
+
+void setup() {
+  myServo.attach(9);
+  
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  potentRead = analogRead(POTENT_PIN);
+  Serial.print("Potentiometer value: "); Serial.print(potentRead);
+  servoAngle = map(potentRead, 0,1023,0,179);
+  Serial.print("'\t Servo Angle: "), Serial.println(servoAngle);
+  myServo.write(servoAngle);
+  delay(15);
+
+}
